52edbf4480aead87317c6536968a9bf9b506bdec
Fernando Felix <fernandojr.ifcg@live.com>
2015-10-18 01:50:45 -0300

    organizando e melhorando codigo, implementando funcao teste

--- src/dfs.py
+++ src/dfs.py
@@ -8,110 +8,65 @@ Adapted from Vicente J. Ferrer Dalmau.
     https://code.google.com/p/tspuib/source/browse/trunk/TravelingSalesMan/src/travelingsalesman/DepthFirstSearch.java?r=13
 '''
 from datetime import datetime
+from graph import Graph, read_graph
 
 
 class DepthFirstSearch:
 
     # Creates a new instance of DepthFirstSearch
-    def __init__(self, distances, sourceCity):
-        self.distances = distances
-        self.sourceCity = sourceCity
-        self.vertices = list(self.distances.keys())
-        print(self.vertices)
+    def __init__(self, start, graph):
+        self.graph = graph  # graph with vertices and edge costs
+        self.start = start  # start city
+        self.vertices = self.graph.vertices()  # cities to visit
         self.optimumRoute = []
-        self.nodes = 0
-        self.routeCost = 0
         self.optimumCost = float('Inf')
 
-    def execute(self):
+    def solve(self):
         '''
         executes the algorithm
         '''
-        initialRoute = [self.sourceCity]
-        self.nodes = self.nodes + 1
+        self.search(self.start)
 
-        startTime = datetime.now()
-        self.search(self.sourceCity, initialRoute)
-        endTime = datetime.now()
-
-        print('DEPTH FIRST SEARCH\n\n')
-        print(
-            "\nBetter solution:",
-            self.optimumRoute,
-            "// Cost:", self.optimumCost, "\n")
-        print("Visited Nodes:", self.nodes, "\n")
-        print("Elapsed Time:", str(endTime - startTime), "ms\n")
-
-    def cost(self, from_node, to_node):
+    def search(self, current, followedRoute=[]):
         '''
-        edge cost (between two nodes)
+         @param current current vertex where we start the search.
+         @param followedRoute for arriving to current vertex.
         '''
-        print(from_node, to_node)
-        return self.distances[from_node][to_node]
+        if current not in followedRoute:
+            followedRoute.append(current)
 
-    def search(self, from_node, followedRoute):
-        '''
-         @param from node where we start the search.
-         @param route followed route for arriving to node "from".
-        '''
+        # we've found a complete route
+        if (len(followedRoute) == len(self.vertices)):
+            followedRoute.append(self.start)  # end city
+            routeCost = self.graph.path_cost(followedRoute)
 
-        # we've found a new solution
-        if (len(followedRoute) == len(self.distances)):
-
-            followedRoute.append(self.sourceCity)
-            self.nodes = self.nodes + 1
-
-            # update the route's cost
-            self.routeCost = self.routeCost + self.cost(from_node, self.sourceCity)
-
-            if (self.routeCost < self.optimumCost):
-                self.optimumCost = self.routeCost
-                self.optimumRoute = list(followedRoute)  # copy
-
-            print(followedRoute, "// Cost:", self.routeCost, "\n")
-
-            # update the route's cost (back to the previous value)
-            self.routeCost = self.routeCost - self.cost(from_node, self.sourceCity)
+            if (routeCost < self.optimumCost):
+                self.optimumCost = routeCost
+                self.optimumRoute = followedRoute
         else:
-            for to_node in self.vertices:
-                if to_node not in followedRoute:
-                    increasedRoute = list(followedRoute)  # copy
-                    increasedRoute.append(to_node)
-                    self.nodes = self.nodes + 1
-
-                    # update the route's cost
-                    self.routeCost = self.routeCost + self.cost(from_node, to_node)
-
-                    self.search(to_node, increasedRoute)
-
-                    # update the route's cost (back to the previous value)
-                    self.routeCost = self.routeCost - self.cost(from_node, to_node)
+            for target in self.vertices:
+                if target not in followedRoute:
+                    self.search(target, list(followedRoute))
 
 
-sourceCity = '0'
-distances = {
-    "0": {
-        "0": 0,
-        "1": 2635.0,
-        "2": 2713.0,
-        "3": 2437.0
-    },
-    "1": {
-        "0": 2635.0,
-        "1": 0,
-        "2": 314.0,
-        "3": 2636.0
-    },
-    "2": {
-        "0": 2713.0,
-        "1": 314.0,
-        "2": 0,
-        "3": 2730.0,
-    },
-    "3": {
-        "0": 2437.0,
-        "1": 2636.0,
-        "2": 2730.0,
-        "3": 0
-    }}
-DepthFirstSearch(distances, sourceCity).execute()
+def test(max_runs=5):
+
+    results = []
+
+    for run in range(max_runs):
+        print('Run:', run)
+        graph = read_graph('data/test.json')
+        graph = Graph(graph)
+        solution = DepthFirstSearch('0', graph)
+        start_time = datetime.now()
+        solution.solve()
+        end_time = datetime.now()
+        elapsed_time = end_time - start_time
+        print("Elapsed Time:", str(elapsed_time), "ms")
+        print("Cost:", solution.optimumCost)
+        print("Path:", solution.optimumRoute)
+        results.append([elapsed_time, solution])
+
+    return results
+
+test()
